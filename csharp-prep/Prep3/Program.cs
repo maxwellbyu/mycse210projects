6ecb249946e9ddaea113da5ff1fac8d07873bc5b
Maxwell Iwe <fxmaxony@gmail.com>
2023-09-21 05:43:35 +0100

    latest update

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,39 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Generate a random magic number between 1 and 100.
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        Console.WriteLine("Welcome to the Guess My Number game!");
+
+        // Start the game loop.
+        while (true)
+        {
+            Console.Write("What is your guess? ");
+            int userGuess;
+            if (int.TryParse(Console.ReadLine(), out userGuess))
+            {
+                if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    break; // Exit the loop when the user guesses correctly.
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
     }
 }
