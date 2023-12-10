a9f64b3ceb226d4497468a265a0dd4af62163b53
maxwellbyu <121932728+maxwellbyu@users.noreply.github.com>
2023-12-10 03:06:31 +0100

    Add files via upload
    Foundation 1 Project

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Collections.Generic;
+
+// Abstraction: Define an abstract class to represent a generic person.
+public abstract class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    // Abstract method for displaying information
+    public abstract void DisplayInfo();
+}
+
+// Inheritance: Student inherits from the Person class.
+public class Student : Person
+{
+    public int StudentId { get; set; }
+
+    // Encapsulation: Properties are encapsulated with private access modifiers.
+    private List<string> courses = new List<string>();
+
+    // Polymorphism: Override the DisplayInfo method in the derived class.
+    public override void DisplayInfo()
+    {
+        Console.WriteLine($"Student: {Name}, Age: {Age}, Student ID: {StudentId}");
+        Console.WriteLine("Courses enrolled:");
+        foreach (var course in courses)
+        {
+            Console.WriteLine($"- {course}");
+        }
+    }
+
+    // Method to enroll in a course
+    public void EnrollInCourse(string course)
+    {
+        courses.Add(course);
+        Console.WriteLine($"{Name} enrolled in {course}.");
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // Creating a student object
+        Student student = new Student
+        {
+            Name = "Maxwell Iwe",
+            Age = 20,
+            StudentId = 12345
+        };
+
+        // Using methods from the base class and derived class
+        student.EnrollInCourse("Math");
+        student.EnrollInCourse("History");
+
+        // Displaying information using polymorphism
+        student.DisplayInfo();
     }
 }
