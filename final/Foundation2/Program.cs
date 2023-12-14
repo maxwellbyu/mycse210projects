658bae5bc141865633b34c2f1f6f859525386ee8
maxwellbyu <121932728+maxwellbyu@users.noreply.github.com>
2023-12-14 11:07:24 +0100

    Add files via upload
    repeat of project 14/12/23

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,114 +1,159 @@
 using System;
-
-// Abstraction: 
-public abstract class CalculatorOperation
-{
-    // Abstract method 
-    public abstract double PerformOperation(double num1, double num2);
-}
-
-// Inheritance:
-public class AdditionOperation : CalculatorOperation
-{
-    public override double PerformOperation(double num1, double num2)
-    {
-        return num1 + num2;
-    }
-}
-
-public class SubtractionOperation : CalculatorOperation
-{
-    public override double PerformOperation(double num1, double num2)
-    {
-        return num1 - num2;
-    }
-}
-
-public class MultiplicationOperation : CalculatorOperation
-{
-    public override double PerformOperation(double num1, double num2)
-    {
-        return num1 * num2;
-    }
-}
-
-public class DivisionOperation : CalculatorOperation
-{
-    public override double PerformOperation(double num1, double num2)
-    {
-        if (num2 != 0)
-        {
-            return num1 / num2;
-        }
-        else
-        {
-            Console.WriteLine("Error: Division by zero is not allowed.");
-            return double.NaN; // NaN (Not a Number) represents an undefined result.
-        }
-    }
-}
-
-// Encapsulation: 
-public class Calculator
-{
-    // Polymorphism: Use an array
-    private CalculatorOperation[] operations;
-
-    public Calculator()
-    {
-        // Initialize operations
-        operations = new CalculatorOperation[]
-        {
-            new AdditionOperation(),
-            new SubtractionOperation(),
-            new MultiplicationOperation(),
-            new DivisionOperation()
-        };
-    }
-
-    // Perform a calculation 
-    public double Calculate(double num1, double num2, int operationChoice)
-    {
-        if (operationChoice >= 0 && operationChoice < operations.Length)
-        {
-            CalculatorOperation selectedOperation = operations[operationChoice];
-            return selectedOperation.PerformOperation(num1, num2);
-        }
-        else
-        {
-            Console.WriteLine("Error: Invalid operation choice.");
-            return double.NaN;
-        }
-    }
-}
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Welcome to the Simple Calculator!");
+        // Create addresses
+        Address address1 = new Address("123 Main St", "CityA", "StateA", "USA");
+        Address address2 = new Address("456 Elm St", "CityB", "StateB", "Canada");
 
-        Console.Write("Enter the first number: ");
-        double num1 = Convert.ToDouble(Console.ReadLine());
+        // Create customers
+        Customer customer1 = new Customer("CustomerA", address1);
+        Customer customer2 = new Customer("CustomerB", address2);
 
-        Console.Write("Enter the second number: ");
-        double num2 = Convert.ToDouble(Console.ReadLine());
+        // Create products
+        Product product1 = new Product("Product1", "P001", 10.99, 3);
+        Product product2 = new Product("Product2", "P002", 5.49, 5);
 
-        Console.WriteLine("Select an operation: ");
-        Console.WriteLine("0. Addition");
-        Console.WriteLine("1. Subtraction");
-        Console.WriteLine("2. Multiplication");
-        Console.WriteLine("3. Division");
+        // Create orders
+        Order order1 = new Order(customer1);
+        order1.AddProduct(product1);
+        order1.AddProduct(product2);
 
-        Console.Write("Enter your choice (0-3): ");
-        int choice = Convert.ToInt32(Console.ReadLine());
+        Order order2 = new Order(customer2);
+        order2.AddProduct(product1);
 
-        // Abstraction and Encapsulation: Create an instance of the Calculator class.
-        Calculator calculator = new Calculator();
+        // Display order information
+        DisplayOrderInformation(order1);
+        DisplayOrderInformation(order2);
+    }
 
-        // Polymorphism: Use the Calculator instance to perform the calculation.
-        double result = calculator.Calculate(num1, num2, choice);
+    static void DisplayOrderInformation(Order order)
+    {
+        Console.WriteLine("Packing Label:");
+        Console.WriteLine(order.GetPackingLabel());
 
-        Console.WriteLine($"The result is: {result}");
+        Console.WriteLine("Shipping Label:");
+        Console.WriteLine(order.GetShippingLabel());
+
+        Console.WriteLine("Total Price: $" + order.GetTotalPrice());
+        Console.WriteLine();
+    }
+}
+
+class Order
+{
+    private List<Product> products = new List<Product>();
+    private Customer customer;
+
+    public Order(Customer customer)
+    {
+        this.customer = customer;
+    }
+
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+    }
+
+    public double GetTotalPrice()
+    {
+        double total = 0;
+        foreach (var product in products)
+        {
+            total += product.GetTotalCost();
+        }
+
+        // Add shipping cost
+        total += customer.IsInUSA() ? 5.0 : 35.0;
+
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "";
+        foreach (var product in products)
+        {
+            label += $"Name: {product.Name}, Product ID: {product.ProductId}\n";
+        }
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"Customer: {customer.Name}\n{customer.Address.GetFullAddress()}";
+    }
+}
+
+class Product
+{
+    private string name;
+    private string productId;
+    private double pricePerUnit;
+    private int quantity;
+
+    public Product(string name, string productId, double pricePerUnit, int quantity)
+    {
+        this.name = name;
+        this.productId = productId;
+        this.pricePerUnit = pricePerUnit;
+        this.quantity = quantity;
+    }
+
+    public double GetTotalCost()
+    {
+        return pricePerUnit * quantity;
+    }
+
+    public string Name { get { return name; } }
+    public string ProductId { get { return productId; } }
+}
+
+class Customer
+{
+    private string name;
+    private Address address;
+
+    public Customer(string name, Address address)
+    {
+        this.name = name;
+        this.address = address;
+    }
+
+    public bool IsInUSA()
+    {
+        return address.IsInUSA();
+    }
+
+    public string Name { get { return name; } }
+    public Address Address { get { return address; } }
+}
+
+class Address
+{
+    private string streetAddress;
+    private string city;
+    private string stateProvince;
+    private string country;
+
+    public Address(string streetAddress, string city, string stateProvince, string country)
+    {
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.stateProvince = stateProvince;
+        this.country = country;
+    }
+
+    public bool IsInUSA()
+    {
+        return country.ToUpper() == "USA";
+    }
+
+    public string GetFullAddress()
+    {
+        return $"{streetAddress}\n{city}, {stateProvince}\n{country}";
     }
 }
